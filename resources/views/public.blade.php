a06fa634759213b62cf52da57785c46f55ab97c5
cbonfa <bonfa@inaum.net>
2022-09-29 15:07:43 -0300

    mudar o @vite

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -1,9 +1,16 @@
 <!doctype html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <!-- Styles -->
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  @livewireStyles
+
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
   <h1 class="text-3xl font-bold underline">
@@ -12,5 +19,6 @@
   <?php
     echo('echo');
   ?>
+  @livewireScripts
 </body>
 </html>

3fc15b5cfd4f16f655650fbba665bfb94e796080
cbonfa <bonfa@inaum.net>
2022-10-04 16:52:46 -0300

    livewire componet

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -40,10 +40,21 @@
         >
             <div class="m-auto bg-pink-500 shadow-2xl rounded-xl p-8">
                 <p class="text-white text-5xl font-extrabold text-center"> Let's do it</p>
-                <form class="flex flex-col items-center p-24">
-                    <x-input class="px-5 py-3 w-80 border border-blue-400" type="email" name="email" placeholder="email address"></x-input>
+                <form 
+                    class="flex flex-col items-center p-24"
+                    wire:submit.prevent="subscribe"
+                >
+                    <x-input 
+                        class="px-5 py-3 w-80 border border-blue-400" 
+                        type="email" 
+                        name="email" 
+                        placeholder="email address"
+                        wire:model="email"
+                    >
+                    </x-input>
                     <span class="text-gray-100 text-xs">We will send you a confirmation e-mail.</span>
-                    <x-button class="px-5 py-3 mt-5 w-80 bg-blue-500 justify-center">Get It</x-button>
+                    <x-button class="px-5 py-3 mt-5 w-80 bg-blue-500 justify-center"
+                    >Get It</x-button>
                 </form>
             </div>
         </div>

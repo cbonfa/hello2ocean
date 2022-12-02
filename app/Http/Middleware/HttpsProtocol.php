76c46457c363e5176a7fe3bc6befb208f2c7bb80
cbonfa <bonfa@inaum.net>
2022-12-02 10:54:18 -0300

    redirect to https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -7,7 +7,7 @@ class HttpsProtocol {
 
     public function handle($request, Closure $next)
     {
-            if (!$request->secure()) {
+            if (!$request->secure() && app()->environment('production')) {
                return redirect()->secure($request->getRequestUri());
             }
 

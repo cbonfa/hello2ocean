a86ed848074435f977f997ef88acb1cb9a993a34
cbonfa <bonfa@inaum.net>
2022-12-13 12:01:00 -0300

    Confirm Delete

--- resources/lang/en/all.php
+++ resources/lang/en/all.php
@@ -5,6 +5,7 @@ return [
     'description' => 'Description',
     'hydrosphere' => 'Hydrosphere',
     'language' => 'Language',
+    'are_you_sure_delete' => 'Are You Sure Delete this Item?',
 
     'choose_your_language' => 'Choose Your Language',
     'show' => 'Show',
--- resources/views/components/button-delete.blade.php
+++ resources/views/components/button-delete.blade.php
@@ -1,3 +1,11 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'w-full inline-block text-center border border-red-500 text-red-500 bg-red-50 hover:bg-red-100 px-2 py-1 no-underline rounded']) }}>
+@props(['confirm' => null])
+
+@php
+    if (!empty($confirm)){
+        $confirm = 'onclick="return confirm(' . "'" . $confirm . "'" . ');"';
+    }
+@endphp
+
+<button {!! $confirm !!} {{ $attributes->merge(['type' => 'submit', 'class' => 'w-full inline-block text-center border border-red-500 text-red-500 bg-red-50 hover:bg-red-100 px-2 py-1 no-underline rounded']) }}>
     {{ $slot }}
 </button>

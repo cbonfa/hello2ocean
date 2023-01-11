9e66692574cadab81423ff3ca11e7f85f91540fa
cbonfa <bonfa@inaum.net>
2023-01-11 17:04:13 -0300

    send reset password

--- app/Http/Controllers/Auth/FisherPasswordResetLinkController.php
+++ app/Http/Controllers/Auth/FisherPasswordResetLinkController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\Fisher;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Auth\Notifications\ResetPassword;
 
 class FisherPasswordResetLinkController extends Controller
 {
@@ -38,6 +42,27 @@ class FisherPasswordResetLinkController extends Controller
         $request->validate([
             'email' => ['required', 'email'],
         ]);
+
+
+        DB::transaction(function() {
+            $fisher = Fisher::whereEmail('fisher@gmail.com')->first();
+            $notification = new ResetPassword;
+            $notification->createUrlUsing(function($notifiable) {
+                return URL::temporarySignedRoute(
+                    'fishers.password.reset',
+                    now()->addMinutes(60),
+                    [
+                            'fisher' => $notifiable->getKey()
+                    ]
+                );
+            });
+            $fisher->notify($notification);
+        }, $deadlockRetries = 5);
+
+
+        $this->reset('email');
+        $this->showFisher = false;
+        $this->showSuccess = true;
 
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
